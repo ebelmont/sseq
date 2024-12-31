5b5e5f203e3b167e2ef05c9d02cc1c2f4ebf46ec
Eva <>
2024-12-31 02:31:50 -0500

    Fix products / compute more products
    Before, we were just computing Ext^0 x Ext^0. Then we tried to compute
    Ext^0 x Ext^{-1} as well. I'm not sure if this was wrong, but I realized
    that we need to compute all Ext^0 x Ext^m. A new argument was added (so
    now you have to input both target spheres of the elements to be
    multiplied) and the logic was reworked.

--- ext/examples/unstable_products.rs
+++ ext/examples/unstable_products.rs
@@ -1,8 +1,9 @@
-//! Computes the product S^{|a|+|c|+n} --c--> S^{|a|+n} --a--> S^n
-//! for a single n and stem(a+c) <= a cutoff
+//! Computing products $E_2^{s_1,t_1}(S^{th_1}) \cdot E_2^(s_2,t_2)(S^{th_2}) \to E_2^{s_1+s_2, t_1+t_2}(S^{th_1})$
+//! for given th1, th2 and all s1, s2, t1, t2.
 //! The output is formatted as tsv, where the fields are:
-//! n, stem of a, filtration of a, basis index of a, stem of c, filtration of c, basis index of c,
-//! vector representing a linear combination of basis elements in the degree of ac
+//! th1, stem of elt1, filtration of elt1, basis index of elt1, th2, stem of elt2, filtration of elt2, basis index of elt2,
+//! vector representing a linear combination of basis elements in the degree of elt1 . elt2.
+//! The order is composition notation order. See the comment below for more on what this means.
 //! 
 
 use std::{path::PathBuf, sync::Arc};
@@ -29,7 +30,7 @@ struct CompositeComputationData {
     resolution1: Arc<UnstableResolution<FiniteChainComplex<SuspensionModule<Box<dyn Module<Algebra = SteenrodAlgebra>>>>>>,
     resolution2: Arc<UnstableResolution<FiniteChainComplex<SuspensionModule<Box<dyn Module<Algebra = SteenrodAlgebra>>>>>>,
     sphere1: i32,
-    offset: i32,
+    sphere2: i32,
 }
 
 
@@ -53,11 +54,15 @@ fn main() -> anyhow::Result<()> {
     let save_dir_name: String = query::raw("Save dir", str::parse);
 
 
-    eprintln!("\nComputing products --c--> --a--> for all c and all a in a range");
+    eprintln!("\nComputing products E_2^(s1,t1)(S^(th1)) . E_2^(s2,t2)(S^(th2))");
+    eprintln!("for your choice of th1 and th2, and all (s1, t1, s2, t2) in a range.");
+    eprintln!("If these are permanent cycles, the product is");
+    eprintln!("S^(th1 + n1 - th2) x S^(th2 + n2) --elt2--> S^(th1 + n1 - th2) x S^(th2) --elt1--> S^(th1)");
+    eprintln!("where n = t-s.\n\n");
 
-    let offset = query::raw("Offset? ", str::parse::<i32>);
+    let sphere1 = query::raw("th1 (sphere for elt1) = ?", str::parse::<std::num::NonZeroI32>).get();
+    let sphere2 = query::raw("th2 (sphere for elt2) = ?", str::parse::<std::num::NonZeroI32>).get();
 
-    let sphere1 = query::raw("sphere of Ext class a", str::parse::<std::num::NonZeroI32>).get();
     let ac_max = Bidegree::n_s(
         query::raw("Max stem", str::parse),
         query::raw("Max filtration", str::parse),
@@ -65,25 +70,25 @@ fn main() -> anyhow::Result<()> {
     
 
     /*
-     * The map of spheres S^{s3} --> S^{s2} --> S^{s1} where s2 = s1 + a.n and s3 = s2 + c.n
-     * S^{|a| + |c| + s1} --> S^{|a| + s1} --> S^{s1} induces a map on
-     * cohomology in the other direction. res1 is the resolution of Sigma^{s1} k and res2 is the
-     * resolution of Sigma^{|a| + s1} k. Write e_n := Sigma^n k.
+     * Write e_n := Sigma^n F_2.
+     * The composition product (a la Bousfield-Kan "Pairings..." section 10) in Adams
+     * grading is
+     * Ext^{th1+n1, f1}(e_{th1}, e_0) . Ext^{th2+n2, f2}(e_{th2}, e_0)
+     * which (section 18) can be expressed as a Yoneda product
+     * Ext^{m,f1}(e_{th1}, e_{th2}) . Ext^{0,f2}(e_{th2}, e_{th2+n2})
+     * --> Ext^{m,f1+f2}(e_{th1}, e_{th2+n2}) where m = th1 - th2 + n1.
+     * Write |a| = (n1, f1), |c| = (n2, f2) (Adams grading).
+     * res1 is the resolution of e_{th1} and res2 is the resolution of e_{th2}.
      * We compute the a map of resolutions
-     * ... --> res1_{c.s + a.s} --> ... --> res1_{a.s} ----> ... --> e_{s1}
-     * ... --> res2_{c.s}  -------> ... --> e_{s2}
-     * ... --> e_{s3}
-     * a is represented by a class in (s,t) = (a.s, s1 + a.t) in res1
-     * c is represented by a class in (s,t) = (c.s, s2 + c.t) in res2
-     * The first map of complexes has degree (s,t) = (a.s, s1 + a.t - s2) = (a.s, a.t - a.n).
-     * The second map of complexes has degree (s,t) = (c.s, s2 + c.t - s3) = (c.s, c.t - c.n).
-     * The product is res1_{c.s + a.s} --> res2_{c.s} --> e_{s3}.
-     *
-     * This is Ext^{0, a.s}(e_{s2}, e_{s1}) x Ext^{0, c.s}(e_{s3}, e_{s2}).
-     * In order to compute the first term in the Leibniz rule, we also need
-     * Ext^{0, a.s}(e_{s2}, e_{s1}) x Ext^{-1, c.s}(e_{s3}, e_{s2}).
-     * In the procedure above, replace the degree of a by (s,t) = (a.s, s1 + a.t - 1)
-     * and the first map of complexes has degree (s,t) = (a.s, a.t - a.n - 1).
+     * ... --> res1_{c.s + a.s} --> ... --> res1_{a.s} ----> ... --> e_{th1}
+     * ... --> res2_{c.s}  -------> ... --> e_{th2}
+     * ... --> e_{th2+c.n}.
+     * a is represented by a class in (s,t) = (a.s, th1 + a.t) in res1
+     * c is represented by a class in (s,t) = (c.s, th2 + c.t) in res2
+     * The first map of complexes has degree (s,t) = (a.s, th1 + a.t - th2) = (a.s, m + a.s).
+     * The second map of complexes has degree (s,t) = (c.s, c.s).
+     * The product is res1_{c.s + a.s} --> res2_{c.s} --> e_{th2+c.n}
+     * and has s = c.s+a.s, t = (th2+c.n) + (m+a.s) + c.s = th1 + a.t + c.t.
      */
 
 
@@ -96,32 +101,30 @@ fn main() -> anyhow::Result<()> {
             save_dir(&save_dir_name, sphere1),
         )?);
 
-    // ac is a map res1_{c.s + a.s} --> Sigma^{sphere3} k and the source degree is
+    let res2: Arc<UnstableResolution<FiniteChainComplex<_>>> =
+        Arc::new(UnstableResolution::new_with_save(
+            Arc::new(FiniteChainComplex::ccdz(Arc::new(SuspensionModule::new(
+                Arc::clone(&module),
+                sphere2
+            )))),
+            save_dir(&save_dir_name, sphere2),
+        )?);
+
+    // ac is a map res1_{c.s + a.s} --> e_{th2+c.n} and the source degree is
     // (a.s + c.s, a.t + c.t + sphere1).
     // We compute this for the maximum values up front.
     let res1_maxdeg = Bidegree::s_t(ac_max.s(), sphere1 + ac_max.t());
     res1.compute_through_stem(res1_maxdeg);
 
     for a_n in 0..ac_max.n() {
-        let sphere2 = sphere1 + a_n;
-        let res2: Arc<UnstableResolution<FiniteChainComplex<_>>> =
-            Arc::new(UnstableResolution::new_with_save(
-                Arc::new(FiniteChainComplex::ccdz(Arc::new(SuspensionModule::new(
-                    Arc::clone(&module),
-                    sphere2
-                )))),
-                save_dir(&save_dir_name, sphere2),
-            )?);
-
-
         for a_s in 0..ac_max.s() {
             //TODO: Check if res1 is zero in degree (s,t) = (a.s, a.t + sphere1)
             let a = Bidegree::n_s(a_n, a_s);
             let cmax_s = ac_max.s() - a.s();
             let cmax_t = ac_max.t() - a.t();
 
-            // c is a map res2_{c.s} --> Sigma^{sphere2}k and the source degree
-            // is (c.s, c.t + sphere2)
+            // c is a map res2_{c.s} --> Sigma^{sphere3}k where sphere3 = sphere2 + c.n
+            // and the source degree is (c.s, c.t + sphere2).
             // We compute the max needed.
             res2.compute_through_stem(Bidegree::s_t(cmax_s, cmax_t + sphere2));
 
@@ -132,7 +135,7 @@ fn main() -> anyhow::Result<()> {
                 resolution1: Arc::clone(&res1),
                 resolution2: Arc::clone(&res2),
                 sphere1,
-                offset,
+                sphere2,
             };
 
 
@@ -142,7 +145,6 @@ fn main() -> anyhow::Result<()> {
                 .try_for_each(|c_n| {
                     compute_composites(c_n, data.clone())
                 })?;
-
         }
     }
     Ok(())
@@ -156,9 +158,10 @@ fn compute_composites(
     let res1 = data.resolution1;
     let res2 = data.resolution2;
     let sphere1 = data.sphere1;
-    let sphere2 = sphere1 + a.n();
+    let sphere2 = data.sphere2;
+    let m = sphere1 - sphere2 + a.n();
+
     let cmax_s = data.ac_max.s() - a.s();
-    let offset = data.offset;
 
 
     for c_s in 0..cmax_s {
@@ -185,8 +188,8 @@ fn compute_composites(
         for c_idx in 0..num_c_classes {
             c_class[c_idx] = 1;
 
-            let ac_deg = Bidegree::s_t(a.s() + c.s(), a.t() + c.t() + sphere1 - offset);
-            let num_gens_a = res1.number_of_gens_in_bidegree(Bidegree::s_t(a.s(), a.t() + sphere1 - offset));
+            let ac_deg = Bidegree::s_t(a.s() + c.s(), a.t() + c.t() + sphere1);
+            let num_gens_a = res1.number_of_gens_in_bidegree(Bidegree::s_t(a.s(), a.t() + sphere1));
             let product_num_gens = res1.number_of_gens_in_bidegree(ac_deg);
             if num_gens_a == 0 || product_num_gens == 0 {
                 continue;
@@ -202,12 +205,12 @@ fn compute_composites(
                     Arc::clone(&res1),
                     Arc::clone(&res2),
                     // hom maps a class of this degree to a class in degree zero
-                    Bidegree::s_t(a.s(), a.t() - a.n() - offset)
+                    Bidegree::s_t(a.s(), (a.s() as i32) + m)
                 ));
 
                 matrix[idx].set_entry(0, 1); // set matrix to the standard basis vector e_idx.
                 hom.extend_step( // this is just mapping res1_{a.s} --> res2_0
-                    Bidegree::s_t(a.s(), a.t() - a.n() + sphere2 - offset), // degree in res1_{a.s} to construct the map (this is the degree that maps to the generator of Sigma^{sphere2}k)
+                    Bidegree::s_t(a.s(), sphere2 + (a.s() as i32) + m), // degree in res1_{a.s} to construct the map (this is the degree that maps to the generator of Sigma^{sphere2}k)
                     Some(&matrix)
                     );
 
@@ -228,11 +231,12 @@ fn compute_composites(
                 let row_trunc = row.slice(0, product_num_gens);
                 if !row_trunc.iter().all(|x| x == 0) {
                     println!(
-                        "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+                        "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
                         sphere1,
                         a.n(),
                         a.s(),
                         i,
+                        sphere2,
                         c.n(),
                         c.s(),
                         c_idx,
